8c4991d5f62c70fe68ff9dc3a45e331a34673d7a
Erik Peterson <Erik.Exists@gmail.com>
2017-01-18 17:19:00 -0800

    Removed explicit sigma from the API
    (use scale_sigma instead)

--- neurons.py
+++ neurons.py
@@ -8,10 +8,8 @@ def lif(time,
         ns,
         ts,
         w_in,
-        w_sigma,
         bias,
-        bias_sigma,
-        f,
+        f=0,
         a=1e-3,
         Nb=1000,
         r_e=40,
@@ -22,25 +20,33 @@ def lif(time,
         tau_i=10e-3,
         g_l=10e-9,
         time_step=1e-4,
+        sigma_scale=10.0,
         report='text'):
     """Create LIF 'computing' neurons"""
     # -----------------------------------------------------------------
     # User params
-
-    w_in = w_in * siemens
-    w_sigma = w_sigma * siemens
-
-    w_e = w_e * siemens
-    w_i = w_i * siemens
     g_l = g_l * siemens
 
+    # comp
+    w_in = w_in * siemens
     w_in = w_in / g_l
-    w_sigma = w_sigma / g_l
+
+    if np.allclose(sigma_scale, 0.0):
+        w_sigma = 0.0
+        bias_sigma = 0.0
+    else:
+        w_sigma = w_in / sigma_scale
+        bias_sigma = bias / sigma_scale
+
+    # noise
+    w_e = w_e * siemens
+    w_i = w_i * siemens
 
     w_e = w_e / g_l
     w_i = w_i / g_l
     g_l = g_l / g_l
 
+    # osc injection
     f *= Hz
     a *= volt
 
@@ -50,12 +56,11 @@ def lif(time,
     # Fixed params
     Et = -54 * mvolt
     Er = -65 * mvolt
-    Ereset = -60 * mvolt
 
     Ee = 0 * mvolt
     Ei = -80 * mvolt
 
-    tau_m = 20 * ms
+    tau_m = 10 * ms
     tau_ampa = tau_e * second
     tau_gaba = tau_i * second
 
@@ -87,19 +92,15 @@ def lif(time,
         refractory=2 * ms,
         method='rk2')
 
-    P_e.v = Ereset
+    P_e.v = Er
 
-    if np.allclose(bias_sigma, 0.0):
-        Is = bias
-    else:
-        Is = np.random.normal(bias, bias_sigma, len(P_e))
+    Is = bias + (bias_sigma * np.random.normal(0, 1, len(P_e)))
     P_e.I = Is * volt
 
     # Set up the 'network'
     # Noise
     C_be = Synapses(P_be, P_e, on_pre='g_e += w_e')
     C_be.connect('i == j')
-
     C_bi = Synapses(P_bi, P_e, on_pre='g_i += w_i')
     C_bi.connect('i == j')
 
@@ -107,7 +108,7 @@ def lif(time,
     P_stim = SpikeGeneratorGroup(k, ns, ts * second)
     C_stim = Synapses(P_stim, P_e, model='w : 1', on_pre='g_in += w')
     C_stim.connect()
-    C_stim.w = 'w_in + (j * w_sigma * randn())'
+    C_stim.w = 'clip(w_in + (j * w_sigma * randn()), 0.0, 1e6)'
 
     # -----------------------------------------------------------------
     # Run, and extract results
