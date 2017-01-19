d1bcc9e82fcda9f69176edc05781b887ef80adb7
Erik Peterson <Erik.Exists@gmail.com>
2017-01-19 15:01:45 -0800

    Intial AdEx working

--- neurons.py
+++ neurons.py
@@ -10,22 +10,14 @@ def lif(time,
         w_in,
         bias,
         f=0,
-        a=1e-3,
-        Nb=1000,
-        r_e=40,
-        r_i=40,
-        w_e=4e-9,
-        w_i=16e-9,
-        tau_e=5e-3,
-        tau_i=10e-3,
-        g_l=10e-9,
+        A=1e-3,
+        r_b=40,
         time_step=1e-4,
         sigma_scale=10.0,
         report='text'):
     """Create LIF 'computing' neurons"""
     # -----------------------------------------------------------------
-    # User params
-    g_l = g_l * siemens
+    g_l = 10e-9 * siemens
 
     # comp
     w_in = w_in * siemens
@@ -39,8 +31,8 @@ def lif(time,
         bias_sigma = bias / sigma_scale
 
     # noise
-    w_e = w_e * siemens
-    w_i = w_i * siemens
+    w_e = 4e-9 * siemens
+    w_i = 16e-9 * siemens
 
     w_e = w_e / g_l
     w_i = w_i / g_l
@@ -48,10 +40,7 @@ def lif(time,
 
     # osc injection
     f *= Hz
-    a *= volt
-
-    r_e = r_e * Hz
-    r_i = r_i * Hz
+    A *= volt
 
     # Fixed params
     Et = -54 * mvolt
@@ -61,13 +50,20 @@ def lif(time,
     Ei = -80 * mvolt
 
     tau_m = 10 * ms
-    tau_ampa = tau_e * second
-    tau_gaba = tau_i * second
+    tau_ampa = 5e-3 * second
+    tau_gaba = 10e-3 * second
 
     time_step *= second
     defaultclock.dt = time_step
 
     # -----------------------------------------------------------------
+    # E/I noise
+    Nb = 1000
+    r_e = r_b * Hz
+    r_i = r_b * Hz
+    P_be = PoissonGroup(Nb, r_e)
+    P_bi = PoissonGroup(Nb, r_i)
+
     # Define neuron and its connections
     lif = """
     dv/dt = (g_l * (Er - v) + I_syn + I_osc + I) / tau_m : volt
@@ -75,13 +71,9 @@ def lif(time,
     dg_in/dt = -g_in / tau_ampa : 1
     dg_e/dt = -g_e / tau_ampa : 1
     dg_i/dt = -g_i / tau_gaba : 1
-    I_osc = a * sin(t * f * 2 * pi) : volt
+    I_osc = A * sin(t * f * 2 * pi) : volt
     I : volt
     """
-
-    # E/I noise
-    P_be = PoissonGroup(Nb, r_e)
-    P_bi = PoissonGroup(Nb, r_i)
 
     # Define the neurons
     P_e = NeuronGroup(
@@ -109,6 +101,123 @@ def lif(time,
     C_stim = Synapses(P_stim, P_e, model='w : 1', on_pre='g_in += w')
     C_stim.connect()
     C_stim.w = 'clip(w_in + (j * w_sigma * randn()), 0.0, 1e6)'
+
+    # -----------------------------------------------------------------
+    # Run, and extract results
+    spikes_e = SpikeMonitor(P_e)
+    traces_e = StateMonitor(P_e, ['v', 'g_in', 'I_osc', 'I'], record=True)
+
+    run(time * second, report=report)
+
+    # Extract spikes
+    ns_e = spikes_e.i_
+    ts_e = spikes_e.t_
+
+    # And the voltages
+    vm = traces_e.v_
+    v_comp = (traces_e.g_in_ * (float(Ee) - vm) + traces_e.I_)
+    v_osc = traces_e.I_osc
+    v_free = vm - float(Et)
+    vs = dict(vm=vm, comp=v_comp, osc=v_osc, free=v_free)
+
+    return ns_e, ts_e, vs
+
+
+def adex(time,
+         N,
+         k,
+         ns,
+         ts,
+         w_in=0.3e-9,
+         bias=5e-5,
+         f=0,
+         A=1e-3,
+         r_b=40,
+         time_step=0.01e-3,
+         sigma_scale=10.0,
+         report='text'):
+    """Create LIF 'computing' neurons"""
+    defaultclock.dt = time_step * second
+
+    C = 281 * pF
+    g_l = 30 * nS
+
+    w_in = w_in * siemens
+    # w_in = w_in / g_l
+
+    # noise
+    w_e = 4e-9 * siemens
+    w_i = 16e-9 * siemens
+
+    # w_e = w_e / g_l
+    # w_i = w_i / g_l
+    # g_l = g_l / g_l
+
+    # osc injection
+    f *= Hz
+    A *= amp
+
+    # neuron kinetics
+    El = -70.6 * mV
+    Et = -50.4 * mV
+    delta_t = 2 * mV
+    tau_w = 40 * ms
+    a = 4 * nS
+    b = 0.08 * nA
+    I = .8 * nA
+    Ecut = Et + 5 * delta_t  # practical threshold condition
+
+    # background synapses
+    Ee = 0 * mvolt
+    Ei = -80 * mvolt
+    tau_ampa = 5e-3 * second
+    tau_gaba = 10e-3 * second
+
+    # -----------------------------------------------------------------
+    # E/I noise
+    Nb = 1000
+    r_e = r_b * Hz
+    r_i = r_b * Hz
+    P_be = PoissonGroup(Nb, r_e)
+    P_bi = PoissonGroup(Nb, r_i)
+
+    # Define neuron and its connections
+    eqs = """
+    dv/dt = (g_l * (El - v) + g_l * delta_t * exp((v - Et) / delta_t) + I_syn + I_osc + I - w) / C : volt
+    dw/dt = (a * (v - El) - w) / tau_w : amp
+    I_syn = g_in * (Ee - v) + g_e * (Ee - v) + g_i * (Ei - v) : amp
+    dg_in/dt = -g_in / tau_ampa : siemens
+    dg_e/dt = -g_e / tau_ampa : siemens
+    dg_i/dt = -g_i / tau_gaba : siemens
+    I_osc = A * sin(t * f * 2 * pi) : amp
+    Er : volt
+    I : amp
+    """
+
+    P_e = NeuronGroup(
+        N,
+        model=eqs,
+        threshold='v > Ecut',
+        reset="v = Er; w += b",
+        method='euler')
+
+    P_e.v = El
+    P_e.w = a * (P_e.v - El)
+    # P_e.Er = linspace(-48.3 * mV, -47.7 * mV, N)  # bifurcation parameter
+    P_e.Er = np.random.normal(-48, 2, N) * mV
+    P_e.I = bias * amp
+
+    # Set up the 'network'
+    # Noise
+    C_be = Synapses(P_be, P_e, on_pre='g_e += w_e')
+    C_be.connect('i == j')
+    C_bi = Synapses(P_bi, P_e, on_pre='g_i += w_i')
+    C_bi.connect('i == j')
+
+    # Stim
+    P_stim = SpikeGeneratorGroup(k, ns, ts * second)
+    C_stim = Synapses(P_stim, P_e, on_pre='g_in += w_in')
+    C_stim.connect()
 
     # -----------------------------------------------------------------
     # Run, and extract results
