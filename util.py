1de92100c600c350c049f67b0b394ed702460272
Erik Peterson <Erik.Exists@gmail.com>
2017-01-20 17:49:13 -0800

    bigfix - adj to changes to fakespikes

--- util.py
+++ util.py
@@ -7,7 +7,7 @@ def k_spikes(t, k, w, dt=1e-3, t_pad=0.1, a=100, seed=42):
     # Generate a rate pulse
     times = util.create_times(t + t_pad, dt)
     nrns = neurons.Spikes(k, t + t_pad, dt=dt, seed=seed)
-    pulse = rates.square_pulse(times, a, t, w, dt)
+    pulse = rates.square_pulse(times, a, t, w, dt, min_a=0)
 
     # Poisson sample the rate timecourse
     ns, ts = util.to_spiketimes(times, nrns.poisson(pulse))
