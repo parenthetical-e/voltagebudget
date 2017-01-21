adbbbb4ce06d0b1cf538aa6212dcf343c48427da
Erik Peterson <Erik.Exists@gmail.com>
2017-01-20 16:09:36 -0800

    Refactored opt fns

--- exp/opt.py
+++ exp/opt.py
@@ -2,11 +2,65 @@
 # import matplotlib.pyplot as plt
 # import numpy as np
 
+import numpy as np
+
 from fakespikes import util as fsutil
 from platypus.algorithms import NSGAII
 from platypus.core import Problem
 from platypus.types import Real
-from voltagebudget import *
+from voltagebudget.neurons import adex, lif
+from voltagebudget.util import k_spikes
+
+
+def create_problem(nrn,
+                   t_stim,
+                   N,
+                   ns,
+                   ts,
+                   f,
+                   w_in=0.1e-9,
+                   bias=5e-3,
+                   pad=10e-3,
+                   Nz=100):
+    time = np.max(ts) + pad
+
+    def problem(A):
+        A = A[0]
+
+        # Create Y, then Z
+        ns_y, ts_y, _ = nrn(time,
+                            N,
+                            ns,
+                            ts,
+                            w_in=w_in,
+                            bias=bias,
+                            f=f,
+                            A=A,
+                            report=None)
+
+        _, ts_z, _ = lif(time,
+                         Nz,
+                         ns_y,
+                         ts_y,
+                         w_in=0.1e-9,
+                         bias=10e-6,
+                         r_b=0,
+                         sigma_scale=10,
+                         f=0,
+                         A=0,
+                         report=None)
+
+        # Est communication
+        m = np.logical_or(t_stim <= ts_z, ts_z <= (t_stim + pad))
+        C = 0
+        if ts_z[m].size > 0:
+            C = ts_z[m].size / float(Nz)
+
+        # Return losses (with C in mimization form)
+        print(A, -C)
+        return A, -C
+
+    return problem
 
 # ---------------------------------------------------------------------
 t = 0.3
@@ -17,15 +71,15 @@ t_stim = 0.1
 dt = 1e-4
 w = 1e-4
 a = 10000
-ns, ts = util.k_spikes(t_stim, k, w, a=a, dt=dt, seed=None)
+ns, ts = k_spikes(t_stim, k, w, a=a, dt=dt, seed=None)
 print(len(ts))
 
 times = fsutil.create_times(t, dt)
 
 # ---------------------------------------------------------------------
 f = 50
-nrn = neurons.lif
-sim = neurons.create_problem(nrn, t_stim, k, ns, ts, f)
+nrn = lif
+sim = create_problem(nrn, t_stim, k, ns, ts, f)
 
 # ---------------------------------------------------------------------
 Amax = 30e-3
