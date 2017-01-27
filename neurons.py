fcfec10c8a328fced5356616c806ee2879e28d42
Erik Peterson <Erik.Exists@gmail.com>
2017-01-26 16:48:37 -0800

    Removed debugging info

--- neurons.py
+++ neurons.py
@@ -117,7 +117,6 @@ def lif(time,
     C_stim = Synapses(P_stim, P_e, model='w : 1', on_pre='g_in += w')
     C_stim.connect()
     C_stim.w = 'clip(w_in + ((j+1)/(j+1) * w_sigma * randn()), 0.0, 1e6)'
-    print(C_stim.w)
 
     # -----------------------------------------------------------------
     # Deinfe variable
