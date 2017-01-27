65b41e72761de516f6edc819855258edb57d8f17
Erik Peterson <Erik.Exists@gmail.com>
2017-01-27 11:23:27 -0800

    Added fn to extact the budget at a fixed time

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from fakespikes import neurons, util, rates
+import numpy as np
 
 
 def k_spikes(t, k, w, dt=1e-3, t_pad=0.1, a=100, seed=42):
@@ -13,3 +14,17 @@ def k_spikes(t, k, w, dt=1e-3, t_pad=0.1, a=100, seed=42):
     ns, ts = util.to_spiketimes(times, nrns.poisson(pulse))
 
     return ns, ts
+
+
+def get_budget(t, times, free, osc, comp):
+
+    if times.shape[0] != free.shape[1]:
+        raise ValueError("times be the same length as the ncol in all"
+                         "other variables (besides t).")
+
+    if (free.shape != osc.shape) or (free.shape != comp.shape):
+        raise ValueError("Shape mismatch in voltage varialbles")
+
+    ind = (np.abs(times - t)).argmin()
+
+    return free[:, ind], osc[:, ind], comp[:, ind]
