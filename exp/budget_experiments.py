eb96d2c6945c18c1a149d5e45e0186f098f19c21
Erik Peterson <Erik.Exists@gmail.com>
2017-10-23 07:45:59 -0700

    working commit

--- exp/budget_experiments.py
+++ exp/budget_experiments.py
@@ -3,11 +3,75 @@ import numpy as np
 from fakespikes.util import spike_window_code
 from fakespikes.util import spike_time_code
 from fakespikes.util import levenstien
+from voltagebudget.neurons import adex
+from voltagebudget.neurons import shadow_adex
+from voltagebudget.util import poisson_impulse
+
+global MODES
+
+MODES = {
+    'regular': {
+        'tau_m': 5e-3,
+        'a': -0.5e-9,
+        'tau_w': 100e-3,
+        'b': 7e-12,
+        'Erheo': -51e-3,
+        'delta_t': 2e-3
+    },
+    'burst': {
+        'tau_m': 5e-3,
+        'a': -0.5e-9,
+        'tau_w': 100e-3,
+        'b': 7e-12,
+        'Erheo': -51e-3,
+        'delta_t': 2e-3
+    },
+}
 
 
-def foward():
+def foward(name,
+           t,
+           t_stim,
+           n,
+           w_in,
+           w_sigma,
+           f=0,
+           A=1e-3,
+           phi=0,
+           mode='regular',
+           seed_value=42,
+           time_step=1e-4):
     """Optimize using voltage budget."""
-    pass
+    np.random.seed(seed_value)
+
+    if mode == 'heterogenous':
+        raise NotImplementedError("TODO")
+    else:
+        params = MODES[mode]
+
+    # IN
+    w = 20e-3
+    a = 60
+    n = 20
+    ns, ts = util.poisson_impulse(t, t_stim, w, a, n, seed=7425)
+
+    # Define individual neurons (by weight)
+    w_ins = np.random.normal(w_in, w_sigma, N)
+    w_ins[w_ins < 0] = 0  # Safety
+
+    # Define ideal targt computation (no oscillation)
+    # (Make sure and explain this breakdown well in th paper)
+    # (it would be an easy point of crit otherwise)
+    ns_opt, ts_opt, budget_opt = adex(t, ns, ts, f=0, A=0, phi=0, **params)
+
+    for w in w_ins:
+        # !
+        ns_osc, ts_osc, budget_osc = adex(
+            t, ns, ts, f=f, A=A, phi=phi, **params)
+
+        # Process budget
+
+        # Score
 
 
 def reverse():
--- util.py
+++ util.py
@@ -7,6 +7,19 @@ import numpy as np
 from copy import deepcopy
 
 
+def poisson_impulse(t, t_stim, w, rate, n, dt=1e-3, seed=None):
+    """Create a pulse of spikes w seconds wide, starting at t_stim."""
+
+    # Poisson sample the rate over w
+    times = fsutil.create_times(t, dt)
+    nrns = neurons.Spikes(n, t, dt=dt, seed=seed)
+    pulse = rates.square_pulse(times, rate, t_stim, w, dt, min_a=0)
+
+    ns, ts = fsutil.to_spiketimes(times, nrns.poisson(pulse))
+
+    return ns, ts
+
+
 def filter_spikes(ns, ts, window):
     m = np.logical_and(ts >= window[0], ts <= window[1])
 
