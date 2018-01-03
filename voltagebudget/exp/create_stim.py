f1b4cbc7f8f235ae7269631f83645db987061a68
Erik Peterson <Erik.Exists@gmail.com>
2018-01-03 14:28:34 -0800

    cleanup imports

--- voltagebudget/exp/create_stim.py
+++ voltagebudget/exp/create_stim.py
@@ -1,29 +1,10 @@
-import json
 import csv
 import os
 import numpy as np
 
 import voltagebudget
-from voltagebudget.neurons import adex
-from voltagebudget.neurons import shadow_adex
 from voltagebudget.util import poisson_impulse
-from voltagebudget.util import read_results
-from voltagebudget.util import read_stim
-from voltagebudget.util import read_args
-from voltagebudget.util import read_modes
 from voltagebudget.util import write_spikes
-
-from voltagebudget.budget import filter_voltages
-from voltagebudget.budget import locate_firsts
-from voltagebudget.budget import locate_peaks
-from voltagebudget.budget import estimate_communication
-from voltagebudget.budget import precision
-
-from voltagebudget.exp import forward
-from voltagebudget.exp import forward_shadow
-from voltagebudget.exp import sweep_power
-from voltagebudget.exp import replay
-from voltagebudget.exp import reverse
 
 
 def create_stim(name,
