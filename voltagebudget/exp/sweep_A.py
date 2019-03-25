d85903362f7044dd0c275dcc28b7a87b2db802fc
Erik Peterson <Erik.Exists@gmail.com>
2019-03-25 16:41:38 -0700

    Added opt_sweep_A; needs testing

--- voltagebudget/exp/sweep_A.py
+++ voltagebudget/exp/sweep_A.py
@@ -27,6 +27,7 @@ from voltagebudget.util import score_by_group
 from voltagebudget.util import score_by_n
 from voltagebudget.util import find_E
 from voltagebudget.util import find_phis
+from voltagebudget.plc import max_deviant
 
 
 def sweep_A(name,
@@ -339,6 +340,221 @@ def sweep_A(name,
     results["biases"] = biases
     results["phis_E"] = phis
     results["phis_w"] = phis_w
+
+    # then write it out.
+    keys = sorted(results.keys())
+    with open("{}.csv".format(name), "w") as fi:
+        writer = csv.writer(fi, delimiter=",")
+        writer.writerow(keys)
+        writer.writerows(zip(*[results[key] for key in keys]))
+
+    # If running in a CL, returns are line noise?
+    if not save_only:
+        return results
+    else:
+        return None
+
+
+def optimal_sweep_A(name,
+                    stim,
+                    E_0,
+                    A_0=0.00e-9,
+                    A_max=0.5e-9,
+                    Z=0.0,
+                    n_samples=10,
+                    t=0.4,
+                    t_pad=0,
+                    d=-5e-3,
+                    w=2e-3,
+                    T=0.0625,
+                    f=8,
+                    N=10,
+                    n_cycles=2,
+                    mode='regular',
+                    sigma=0,
+                    w_in_0=None,
+                    w_in_max=None,
+                    no_lock=False,
+                    verbose=False,
+                    save_only=False,
+                    save_spikes=False,
+                    save_traces=False,
+                    seed_value=42):
+    """Optimize using the shadow voltage budget."""
+    np.random.seed(seed_value)
+
+    # --------------------------------------------------------------
+    # Temporal params
+    t = t + t_pad
+    time_step = 1e-5
+
+    # --------------------------------------------------------------
+    if verbose:
+        print(">>> Setting mode.")
+
+    params, w_in, bias_in, _ = read_modes(mode)
+
+    # Overide default w_in?
+    if w_in_0 is not None:
+        w_in[0] = w_in_0
+    if w_in_max is not None:
+        w_in[1] = w_in_max
+
+    # --------------------------------------------------------------
+    if verbose:
+        print(">>> Importing stimulus from {}.".format(stim))
+
+    stim_data = read_stim(stim)
+    ns = np.asarray(stim_data['ns'])
+    ts = np.asarray(stim_data['ts'])
+
+    # --------------------------------------------------------------
+    # Define target computation (i.e., no oscillation)
+    # (TODO Make sure and explain this breakdown well in th paper)
+    if verbose:
+        print(">>> Creating reference spikes.")
+
+    ns_ref, ts_ref, voltages_ref = adex(
+        N,
+        t,
+        ns,
+        ts,
+        w_in=w_in,
+        bias_in=bias_in,
+        f=0.0,
+        A=0,
+        phi=0,
+        sigma=sigma,
+        budget=True,
+        save_args=None,
+        time_step=time_step,
+        seed_value=seed_value,
+        **params)
+
+    if ns_ref.size == 0:
+        raise ValueError("The reference model didn't spike.")
+
+    # --------------------------------------------------------------
+    # Find E and phis
+    E = find_E(E_0, ns_ref, ts_ref, no_lock=no_lock, verbose=verbose)
+    phi_w, phi_E = find_phis(E, f, d, verbose=verbose)
+
+    # Filter ref spikes into the window of interest
+    ns_ref, ts_ref = filter_spikes(ns_ref, ts_ref, (E, E + T))
+    var_ref = mad(ts_ref)
+
+    if verbose:
+        print(">>> {} spikes in the analysis window.".format(ns_ref.size))
+
+    if save_spikes:
+        write_spikes("{}_ref_spks".format(name), ns_ref, ts_ref)
+
+    # --------------------------------------------------------------
+    # Rank the neurons
+
+    # --------------------------------------------------------------
+    # Init results
+    neurons = []
+    ranks = []
+
+    variances_pop = []
+    errors_pop = []
+    errors_opt = []
+    n_spikes_pop = []
+    phis = []
+
+    # --------------------------------------------------------------
+    # Run samples
+    samples = np.linspace(A_0, A_max, n_samples)
+
+    for i, A_i in enumerate(samples):
+        # -
+        # Sat homeostasis factor
+        bias_adj = bias_in - (A_i * Z)
+
+        if verbose:
+            print(">>> Running A {:0.15f} ({}/{}).".format(
+                A_i, i + 1, n_samples))
+            print(">>> (bias_in {}) -> (bias_adj {})".format(
+                bias_in, bias_adj))
+
+        # -
+        # Run
+        # Spikes, using phi_E
+        ns_i, ts_i = adex(
+            N,
+            t,
+            ns,
+            ts,
+            w_in=w_in,
+            bias_in=bias_adj,
+            f=f,
+            A=A_i,
+            phi=phi_E,
+            E=E,
+            n_cycles=n_cycles,
+            sigma=sigma,
+            budget=False,
+            seed_value=seed_value,
+            time_step=time_step,
+            save_args=None,
+            **params)
+
+        # -
+        # Analyze!
+        # Filter spikes in E
+        ns_i, ts_i = filter_spikes(ns_i, ts_i, (E, E + T))
+
+        # Pop var and error
+        var_pop = mad(ts_i)
+        _, error_pop = score_by_n(N, ns_ref, ts_ref, ns_i, ts_i)
+        n_spikes = ts_i.size
+
+        # Best possible pop error
+        percent_change = (var_pop - var_ref) / var_pop
+        if percent_change < 0:
+            ts_opt = ts_i
+            errors_opt = 0.0
+        else:
+            _, _, _, ts_opt = max_deviant(
+                ts_i, percent_change, side='both', mode_fm=np.median, dt=dt)
+            _, error_opt = score_by_n(N, ns_ref, ts_ref, ns_i, ts_opt)
+
+        # -
+        # Save
+        variances_pop.append(var_pop)
+        errors_pop.append(error_pop)
+        errors_opt.append(error_opt)
+        n_spikes_pop.append(n_spikes)
+
+        # -
+        if verbose:
+            print(
+                ">>> (A {:0.12f})  ->  (N spks, {}, mae {:0.5f}, mad, {:0.5f})"
+                .format(A_i, ns_i.size / float(N), error_pop, var_pop))
+
+        if save_spikes:
+            print(">>> Writing details for A {} (nA)".format(
+                np.round(A_i * 1e9, 3)))
+        if save_spikes:
+            write_spikes("{}_A{}_spks".format(name, np.round(A_i * 1e9, 3)),
+                         ns_i, ts_i)
+            write_spikes(
+                "{}_A{}_spks_opt".format(name, np.round(A_i * 1e9, 3)), ns_i,
+                ts_opt)
+
+    # --------------------------------------------------------------
+    if verbose:
+        print(">>> Saving results.")
+
+    # Build a dict of results,
+    results = {}
+    results["variances_pop"] = variances_pop
+    results["errors_pop"] = errors_pop
+    results["n_spikes_pop"] = n_spikes_pop
+    results["errors_opt"] = errors_opt
+    results["As"] = samples
+    results["phis_E"] = phis
 
     # then write it out.
     keys = sorted(results.keys())
